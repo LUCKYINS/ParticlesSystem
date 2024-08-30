43a89b5804dd887b0f479f745aa9e03ad61904fd
LUCKYINS <elhusseinabdalrahman@gmail.com>
2024-08-31 00:47:32 +0200

    update main.js
    - Side movement
    - Particles number input
    - width limit
    TODO height limit, direction and velocity input

--- main.js
+++ main.js
@@ -2,15 +2,25 @@
     Coder: LUCKYINS
 */
 
-// Getting the canvas
+//Html Elements
 const canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const particlesNumber = document.getElementById("particleNumber");
+
+// Getting the canvas
+let cWidth = window.innerWidth;
+let cHeight = window.innerHeight;
+canvas.width = cWidth;
+canvas.height = cHeight
+
+// Input Value
+let particlesNumberValue = -1;
+let velocityValue = -1;
 
 // Canvas context
 const ctx = canvas.getContext("2d");
 color = Array("blue", "red", "cyan", "white", "pink", "green");
 direction = Array() //TODO
+
 
 // Uniform rectilinear movement
 function URM(x, v){
@@ -20,11 +30,14 @@ function URM(x, v){
 //Particle class
 class Particle{
     constructor(x, y, radius, color,velocity){
-        this._x = x;
-        this._y = y;
+        // Assure that the position doesn't go beyond the canvas limit
+        this._x = (x+radius < cWidth && x-radius> 0) ? x : (x+radius> cWidth)  ? x-radius : x+radius; 
+        this._y = (y+radius < cHeight && y-radius> 0) ? y : (y+radius> cHeight) ? y-radius : y+radius;
+
         this._radius = radius;
         this._color = color;
         this._velocity = velocity
+    
     }
     getX(){
         return this._x;
@@ -56,19 +69,30 @@ class ParticlesGenerator{
     makeParticles(){
         for (let i= 0; i < this._number; i++){
             // Create an Array of particles
-            this._particlesArray.push(new Particle(window.innerWidth*Math.random(), window.innerHeight*Math.random(), Math.random()*20+20, color[i%5], 2))
+            this._particlesArray.push(new Particle(window.innerWidth*Math.random(), window.innerHeight*Math.random(), 20, color[i%5], 1))
         }
     }
 
     // Update particle position
     updateParticles(particle){
         //movement
-        if (particle._x < window.innerWidth){
-            particle._x = URM(particle._x, particle.getVelocity())
+        if (particle.getVelocity()> 0){
+            if (particle._x+particle._radius < window.innerWidth){
+                particle._x = URM(particle._x, particle.getVelocity())
+            }
+            else{
+                particle._velocity *=-1
+            }
         }
-        if (particle._y < window.innerWidth){
-            particle._y = URM(particle._y, particle.getVelocity())
+        if (particle.getVelocity()< 0){
+            if (particle._x-particle._radius > 0){
+                particle._x = URM(particle._x, particle.getVelocity())
+            }
+            else{
+                particle._velocity *=-1
+            }
         }
+        // TODO height limit
     }
 
     //set Particles
@@ -90,14 +114,21 @@ class ParticlesGenerator{
 }
 
 
-P1 = new ParticlesGenerator(100)
+// Create Particles
+let P1 = new ParticlesGenerator(particlesNumberValue);
 
 // Render Animation
 function render(){
+    // Change number value of particles
+    if (particlesNumberValue != particlesNumber.value){
+        P1 = new ParticlesGenerator(particlesNumberValue);
+        particlesNumberValue = particlesNumber.value;
+    }
+    // Erase Frame
     ctx.clearRect(0,0,window.innerWidth, window.innerHeight)// Erase
     // Draw
     P1.setParticles()
-    // Draw
-    requestAnimationFrame(render)// loop
+    // loop
+    requestAnimationFrame(render)
 }
 render()
