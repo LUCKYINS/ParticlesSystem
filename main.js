ac6d46011fdae1256d5916bc2dd34c144df257c3
LUCKYINS <148013915+LUCKYINS@users.noreply.github.com>
2024-09-15 01:20:09 +0200

    update main.js
    Random movement

--- main.js
+++ main.js
@@ -14,12 +14,11 @@ canvas.height = cHeight
 
 // Input Value
 let particlesNumberValue = -1;
-let velocityValue = -1;
+let vxValue = -1;
 
 // Canvas context
 const ctx = canvas.getContext("2d");
 color = Array("blue", "red", "cyan", "white", "pink", "green");
-direction = Array() //TODO
 
 
 // Uniform rectilinear movement
@@ -29,14 +28,15 @@ function URM(x, v){
 
 //Particle class
 class Particle{
-    constructor(x, y, radius, color,velocity){
+    constructor(x, y, radius, color,vx, vy){
         // Assure that the position doesn't go beyond the canvas limit
         this._x = (x+radius < cWidth && x-radius> 0) ? x : (x+radius> cWidth)  ? x-radius : x+radius; 
         this._y = (y+radius < cHeight && y-radius> 0) ? y : (y+radius> cHeight) ? y-radius : y+radius;
 
         this._radius = radius;
         this._color = color;
-        this._velocity = velocity
+        this._vx = vx;
+        this._vy = vy;
     
     }
     getX(){
@@ -51,8 +51,11 @@ class Particle{
     getColor(){
         return this._color;
     }
-    getVelocity(){
-        return this._velocity;
+    getVX(){
+        return this._vx;
+    }
+    getVY(){
+        return this._vy;
     }
 }
 
@@ -69,30 +72,52 @@ class ParticlesGenerator{
     makeParticles(){
         for (let i= 0; i < this._number; i++){
             // Create an Array of particles
-            this._particlesArray.push(new Particle(window.innerWidth*Math.random(), window.innerHeight*Math.random(), 20, color[i%5], 1))
+            let x = window.innerWidth*Math.random()
+            let y = window.innerHeight*Math.random()
+            let radius = 15 + Math.random()*20 
+            let pColor = color[i%5]
+            let vx = (Math.random() >= 0.5) ? Math.random()*5 : - Math.random()*5
+            let vy = (Math.random() >= 0.5) ? Math.random()*5 : - Math.random()*5
+            this._particlesArray.push(new Particle(x, y, radius, pColor , vx, vy))
         }
     }
 
     // Update particle position
     updateParticles(particle){
         //movement
-        if (particle.getVelocity()> 0){
+        if (particle.getVX()> 0){
             if (particle._x+particle._radius < window.innerWidth){
-                particle._x = URM(particle._x, particle.getVelocity())
+                particle._x = URM(particle._x, particle.getVX())
             }
             else{
-                particle._velocity *=-1
+                particle._vx *=-1
             }
         }
-        if (particle.getVelocity()< 0){
+        if (particle.getVX()< 0){
             if (particle._x-particle._radius > 0){
-                particle._x = URM(particle._x, particle.getVelocity())
+                particle._x = URM(particle._x, particle.getVX())
             }
             else{
-                particle._velocity *=-1
+                particle._vx *=-1
             }
         }
-        // TODO height limit
+        if (particle.getVY()> 0){
+            if (particle._y+particle._radius < window.innerHeight){
+                particle._y = URM(particle._y, particle.getVY())
+            }
+            else{
+                particle._vy *=-1
+            }
+        }
+        if (particle.getVY()< 0){
+            if (particle._y-particle._radius > 0){
+                particle._y = URM(particle._y, particle.getVY())
+            }
+            else{
+                particle._vy *=-1
+            }
+        }
+
     }
 
     //set Particles
@@ -106,7 +131,7 @@ class ParticlesGenerator{
             let x = particle.getX()
             let y = particle.getY()
             ctx.beginPath()
-            ctx.arc(x,y,particle.getRadius(), 0, 2*Math.PI , true)
+            ctx.arc(x,y,particle.getRadius(), 0, 2 * Math.PI , true)
             ctx.fill()
             this.updateParticles(particle)
         }
